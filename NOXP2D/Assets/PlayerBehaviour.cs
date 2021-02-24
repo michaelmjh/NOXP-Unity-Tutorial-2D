1d1fe23846903fd1e64a74082e69c49edcebee1e
michaelmjh <michaelmjh@icloud.com>
2021-02-24 17:47:17 +0000

    player, movement and background

---
+++ NOXP2D/Assets/PlayerBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    [SerializeField] float speed = 0.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        float maxDistanceToTravel = speed * Time.deltaTime;
+
+        transform.position += Vector3.up * Input.GetAxis("Vertical") * speed;
+        transform.position += Vector3.right * Input.GetAxis("Horizontal") * speed;
+    }
+}
